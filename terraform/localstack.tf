72f00e7247b26544ffcb2fba0de5a893a5f818be
Melon <m.melon@m2amedia.tv>
2020-08-27 00:02:51 +0600

    Added terraform for localstack

---
+++ terraform/localstack.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region                      = "eu-west-1"
+  access_key                  = "fakekey"
+  secret_key                  = "fakekey"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+
+  endpoints {
+    lambda         = "http://localhost:4574"
+  }
+}
