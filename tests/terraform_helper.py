c8ca87e445a670a149ac9e2f398ff32c9e957dff
Melon <m.melon@m2amedia.tv>
2020-08-27 02:31:57 +0600

    Terraform test helper file added

---
+++ tests/terraform_helper.py
@@ -0,0 +1,45 @@
+"""Module for terraform utility methods."""
+import subprocess
+
+TERRAFORM_DIR_PATH = "../terraform/"
+
+
+def terraform_init():
+    """Terraform init command"""
+    tf_init = ["terraform", "init", TERRAFORM_DIR_PATH]
+    subprocess.check_call(tf_init)
+
+
+def create_resources():
+    """Create a tf resource."""
+    proc = subprocess.Popen("terraform apply -auto-approve " + TERRAFORM_DIR_PATH, shell=True)
+    proc.wait()
+
+
+def destroy_resources():
+    """Destroy all tf resources.
+
+    This method will destroy any resources it can find in the state file,
+    and delete all resources from the state file.
+    """
+    tf_destroy = [
+        "terraform",
+        "destroy",
+        "-auto-approve",
+        TERRAFORM_DIR_PATH
+    ]
+    subprocess.call(tf_destroy)
+
+    tf_refresh = [
+        "terraform",
+        "refresh",
+        TERRAFORM_DIR_PATH
+    ]
+    subprocess.call(tf_refresh)
+
+
+def terraform_start():
+    """ teardown and create resources at the beginning of feature test """
+    terraform_init()
+    destroy_resources()
+    return create_resources()
